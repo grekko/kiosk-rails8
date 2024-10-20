54b987c22596ad0ef3997b0411209a03513ea1d7
Gregory Igelmund <github.com@grekko.de>
2024-10-20 23:00:55 +0200

    Add Finance section

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -3,5 +3,8 @@ class InventoryController < ApplicationController
     @ordered_drink_count = OrderPosition.group(:drink_id).sum(:amount)
     @settled_drink_count = SettlementPosition.group(:drink_id).sum(:amount)
     @drinks = Drink.all
+
+    @ordered_in_cents = OrderPosition.sum(:price_in_cents)
+    @settled_in_cents = SettlementPosition.sum(:price_in_cents)
   end
 end
