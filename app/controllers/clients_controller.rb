69a41dce7c041920e94ce28d2a75fb7782ae4a87
Gregory Igelmund <github.com@grekko.de>
2024-10-20 16:48:56 +0200

    Create Clients

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,48 @@
+class ClientsController < ApplicationController
+  before_action :set_client, only: %i[ edit update destroy ]
+
+  def index
+    @clients = Client.all
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def edit
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to @client, notice: "Client was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to clients_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @client.destroy!
+
+    redirect_to clients_path, status: :see_other, notice: "Client was successfully destroyed."
+  end
+
+  private
+
+  def set_client
+    @client = Client.find(params.expect(:id))
+  end
+
+  def client_params
+    params.expect(client: [ :name ])
+  end
+end
