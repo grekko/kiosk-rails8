23c080adb3b5fe5094d1f2573c39681da0a5eea7
Gregory Igelmund <github.com@grekko.de>
2024-10-29 06:47:12 +0100

    Inventory: Add settlement details, add details about total inventory

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -5,6 +5,7 @@ class InventoryController < ApplicationController
     @drinks = Drink.all
 
     @ordered_in_cents = OrderPosition.sum(:price_in_cents)
-    @settled_in_cents = SettlementPosition.sum(:price_in_cents)
+    @settled_in_cents = SettlementPosition.joins(:settlement).where(settlements: { aasm_state: :paid }).sum(:price_in_cents)
+    @unsettled_in_cents = SettlementPosition.joins(:settlement).where(settlements: { aasm_state: :completed }).sum(:price_in_cents)
   end
 end
