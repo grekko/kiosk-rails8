b9af699836db32786fb451aeb914d11c1dc628ed
Gregory Igelmund <github.com@grekko.de>
2025-11-09 10:54:36 +0100

    MonthlyReport: Add some checks

--- app/models/monthly_report.rb
+++ app/models/monthly_report.rb
@@ -2,6 +2,10 @@ class MonthlyReport < ApplicationRecord
   has_many :settlements
   has_one_attached :image
 
+  def overall_amount
+    settlements.flat_map(&:positions).sum(&:amount)
+  end
+
   def complete_settlements!
     settlements.draft.find_each do |settlement|
       settlement.complete!
