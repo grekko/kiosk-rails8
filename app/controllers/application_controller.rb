5a29a52808bc8287749f8021e5c4073b1ff6c345
Gregory Igelmund <github.com@grekko.de>
2025-11-27 23:13:26 +0100

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  http_basic_authenticate_with name: "kiosk", password: "sup3r-s3cr37"
 end
